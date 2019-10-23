2bcd8a183ecee8a108c9483419a1fd1fc723b1ac
gorniak <gorniakowski@gmail.com>
2019-10-23 15:47:31 +0200

    fix normalize bug, start implementing crypto algorithm

--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -1,14 +1,36 @@
 package cryptosquare
 
-import "unicode"
+import (
+	"fmt"
+	"unicode"
+)
 
 func normalize(input string) (result string) {
 
 	for _, char := range input {
-		if unicode.IsDigit(char) && unicode.IsLetter(char) {
+		if unicode.IsDigit(char) || unicode.IsLetter(char) {
 			char = unicode.ToLower(char)
 			result += string(char)
+
 		}
 	}
 	return
+}
+
+type Rectangle [][]rune
+
+func Encode(input string) string {
+	input = normalize(input)
+
+	c, r := 1, 0
+	for {
+
+		if c >= r && c-r <= 1 && c*r >= len(input) {
+			break
+		}
+		c++
+		r++
+	}
+	fmt.Println(c, r)
+	return ""
 }
