5cb8679f7f30843c596efaa3566fe010226cc20e
gorniak <gorniakowski@gmail.com>
2019-11-09 19:23:43 +0100

    sieve done

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,39 @@
 package sieve
 
-func Sieve(limit int) []int {
+import (
+	"math"
+)
 
+type pair struct {
+	number int
+	mark   bool
+}
+
+func Sieve(limit int) (result []int) {
+
+	if limit == 1 {
+		return []int{}
+	}
+	table := []pair{}
+
+	for i := 2; i <= limit; i++ {
+		table = append(table, pair{i, true})
+
+	}
+
+	for i := 2; float64(i) <= math.Sqrt(float64(limit)); i++ {
+		if table[i].mark {
+			s := 0
+			for j := i * i; j <= limit; j = (i * i) + (s * i) {
+				s++
+				table[j-2].mark = false
+			}
+		}
+	}
+	for _, item := range table {
+		if item.mark {
+			result = append(result, item.number)
+		}
+	}
+	return
 }
