ef704001dd6011254c57889be40e63e5689e66c6
gorniak <gorniakowski@gmail.com>
2019-11-21 12:20:47 +0100

    series finished

--- series/series.go
+++ series/series.go
@@ -1,1 +1,21 @@
 package series
+
+func All(n int, s string) []string {
+
+	result := make([]string, 0)
+	for i := 0; n+i <= len(s); i++ {
+		result = append(result, s[i:n+i])
+	}
+	return result
+}
+
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
+}
+
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	}
+	return s[:n], true
+}
